8c09aeccf3416c8d12b225dd113b46cb99e98612
AdrianS95 <adrian.smaza2@gmail.com>
2018-05-28 22:26:19 +0200

    add viev login and register

---
+++ NVC_Food_APP/NVC_Food_APP/Controllers/KontaController.cs
@@ -0,0 +1,53 @@
+﻿using NVC_Food_APP.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NVC_Food_APP.Controllers
+{
+    public class KontaController : Controller
+    {
+        // GET: Konta
+        public ActionResult Logowanie(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logowanie(LogowanieViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+        public ActionResult Rejestracja (string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Rejestracja(LogowanieViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+
+    }
+}
---
+++ NVC_Food_APP/NVC_Food_APP/ViewModel/KontaViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NVC_Food_APP.ViewModel
+{
+    public class LogowanieViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+    }
+    public class RejestracjaViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "{0} musi mieć co najmniej {2} znaków.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = " Hasło ")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdz Hasło ")]
+        [Compare("Password", ErrorMessage = "Hasło i potwierdzenie hasła nie pasują do siebie.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Adres { get; set; }
+
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string Miasto { get; set; }
+
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string KodPocztowy { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
