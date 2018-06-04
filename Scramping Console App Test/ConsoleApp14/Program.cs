bd7d448fa290efee14ee0194baeeb7a6b0467193
AdrianS95 <adrian.smaza2@gmail.com>
2018-06-04 23:23:37 +0200

    Scramping

--- Scramping Console App Test/ConsoleApp14/Program.cs
+++ Scramping Console App Test/ConsoleApp14/Program.cs
@@ -9,54 +9,130 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp14
 {
+    public class Model
+    {
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public string Cena { get; set; }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //var url = "https://www.pyszne.pl/bracia";
-            var url = "https://www.pyszne.pl/restauracyjka-zacisze";
-            //var url = "https://www.pyszne.pl/alle-kebab";
+            //tring url = "https://www.pyszne.pl/bracia";
+            //var url = "https://www.pyszne.pl/restauracyjka-zacisze";
+            var url = "https://www.pyszne.pl/alle-kebab";
             //var url = "https://www.pyszne.pl/marwex-andrychow";
-
-            // Działa tylko dla restauracji z pyszne.pl
-            
-            //To do: Uwaga na puste opisy 
-
+            Console.WriteLine(DateTime.Now.Ticks);
             var webGet = new HtmlWeb();
             if (webGet.Load(url) is HtmlDocument document)
             {
-                /////Nazwa
 
-                var nazwa = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").CssSelect(".meal-name").ToArray();
+                var sektor = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").ToArray();
+                var mod = new List<Model>();
 
-                foreach (var index in nazwa)
-                {
-                    Console.WriteLine("Pizza: " + index.InnerText.ToString().Trim());
-                }
 
-                //////Opis
 
-                var opis = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").CssSelect(".meal-description-additional-info").ToArray();
-                foreach (var index in opis)
-                {
-                    Console.WriteLine("nazwa: " + index.InnerText.ToString().Trim());
-                }
+                    foreach (var item in sektor)
+                    {
+                        var nazwa = item.CssSelect(".meal-name");
+                        var opis = item.CssSelect(".meal-description-additional-info");
+                        var cena = item.CssSelect(".meal-add-btn-wrapper");
 
-                //////cana
+                        foreach (var i in nazwa)
+                        {
+                            Console.WriteLine(i.InnerText.Trim());
+                        }
 
-                var cena = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").CssSelect(".meal-add-btn-wrapper").ToArray();
-                foreach (var index in cena)
-                {
-                    Console.WriteLine("Cena: " + index.InnerText.ToString().Trim());
-                }
+                        foreach (var i in opis)
+                        {
+                            Console.WriteLine(i.InnerText.Trim());
+                        }
 
-            Console.WriteLine($"Nazw - { nazwa.Count()}   Opisów - {opis.Count()}  cen - {cena.Count()}");
+                        foreach (var i in cena)
+                        {
+                            Console.WriteLine(i.InnerText.Trim());
+                        }
+
+
+                        Console.WriteLine();
+
+                    }
 
             }
 
 
+            //var webGet = new HtmlWeb();
+            //if (webGet.Load(url) is HtmlDocument document)
+            //{
+            //    var sektor = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").ToArray();
 
+            //    foreach (var item in sektor)
+            //    {
+            //        var nazwa = item.CssSelect(".meal-name");
+            //        var opis = item.CssSelect(".meal-description-additional-info");
+            //        var cena = item.CssSelect(".meal-add-btn-wrapper");
+
+            //        foreach (var i in nazwa)
+            //        {
+            //            Console.WriteLine(i.InnerText.Trim());
+            //        }
+
+            //        foreach (var i in opis)
+            //        {
+            //            Console.WriteLine(i.InnerText.Trim());
+            //        }
+
+            //        foreach (var i in cena)
+            //        {
+            //            Console.WriteLine(i.InnerText.Trim());
+            //        }
+
+            //        Console.WriteLine();
+
+            //    }
+
+
+
+
+
+            /////Nazwa
+
+            //var nazwa = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").CssSelect(".meal-name").ToArray();
+
+            //foreach (var index in nazwa)
+            //{
+            //    Console.WriteLine("Pizza: " + index.InnerText.ToString().Trim());
+            //}
+
+            //////Opis
+
+            //var opis = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").CssSelect(".meal-description-additional-info").ToArray();
+            //foreach (var index in opis)
+            //{
+            //    Console.WriteLine("nazwa: " + index.InnerText.ToString().Trim());
+            //}
+
+            //////cana
+
+            //var cena = document.DocumentNode.CssSelect(".menucard").CssSelect(".meal").CssSelect(".meal-add-btn-wrapper").ToArray();
+            //foreach (var index in cena)
+            //{
+            //    Console.WriteLine("Cena: " + index.InnerText.ToString().Trim());
+            //}
+
+            //Console.WriteLine($"Nazw - { nazwa.Count()}   Opisów - {opis.Count()}  cen - {cena.Count()}");
+            Console.WriteLine(DateTime.Now.Ticks);
             Console.ReadLine();
         }
+
     }
+
 }
+
+
+
+
+
